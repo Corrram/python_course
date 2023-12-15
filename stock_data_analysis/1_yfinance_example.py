e61e22a04f280160cd3c7c079bca0a77158b11a9
Marcus Rockel <marcusrockel@icloud.com>
2023-12-15 11:53:09 +0100

    Refactor fetch_sp500_tickers into separate module
    The `fetch_sp500_tickers` function, which scrapes S&P 500 tickers from Wikipedia, has been moved from the `1_yfinance_example.py` script into a newly created `0_requests_example.py` module. Additionally, the `requests` library has been added to `requirements.txt`. This refactoring improves code organization and ensures necessary dependencies are included in our requirements file.

--- stock_data_analysis/1_yfinance_example.py
+++ stock_data_analysis/1_yfinance_example.py
@@ -1,30 +1,6 @@
 import pathlib
 
-import bs4 as bs
-import requests
 import yfinance as yf
-
-
-def fetch_sp500_tickers():
-    """
-    Fetches the S&P 500 tickers from Wikipedia and saves them to a file.
-    Code adapted from
-    https://stackoverflow.com/questions/58890570/python-yahoo-finance-download-all-sp-500-stocks
-
-    :return:
-    """
-    resp = requests.get("http://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-    soup = bs.BeautifulSoup(resp.text, "lxml")
-    table = soup.find("table", {"class": "wikitable sortable"})
-    tickers = []
-    for row in table.findAll("tr")[1:]:
-        ticker = row.findAll("td")[0].text
-        tickers.append(ticker)
-    tickers = [s.replace("\n", "") for s in tickers]
-    with open("sp500-ticker-list.txt", "w") as f:
-        f.write("\n".join(tickers))
-
-    return tickers
 
 
 def download_stock_data(tickers=None):
